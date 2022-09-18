4f6cc9530153c16f9ca9aacef73744e7fe2a9132
SDTakeuchi <kuranku191952996@gmail.com>
2022-09-18 21:28:13 +0900

    created unittests

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,9 +1,10 @@
 package db
 
 import (
-	"os"
-	"log"
 	"database/sql"
+	"fmt"
+	"log"
+	"os"
 	"testing"
 
 	_ "github.com/lib/pq"
@@ -13,11 +14,21 @@ var testQueries *Queries
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	// dbSource = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var dbConfig = fmt.Sprintf(
+	"host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+	"postgres",
+	"5432",
+	"root",
+	"simple_bank",
+	"secret",
+	"disable",
 )
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(dbDriver, dbConfig)
 	if err != nil {
 		log.Fatal("cannot connect to database")
 	}
